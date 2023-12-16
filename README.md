1151acf6e900258c252455ebfba283e8a3157b6c
elisabeth <elisabeth.r2ni@gmail.com>
2023-12-16 20:15:35 +0200

    Add post creation endpoint

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wad-team-2-node
 wad team 2 back end
+
+# Endpoint demos
+
+## Create a new post
+
+curl -i -X POST localhost:3000/posts -d '{"authorName": "nimi", "authorEmail": "email", "text": "lalala"}' -H "Content-Type: application/json"
--- server.js
+++ server.js
@@ -10,3 +10,22 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Server is listening to port ${port}`);
 });
+
+app.post('/posts', async (req, res) => {
+  try {
+    const { authorName, authorEmail, text } = req.body;
+    const newPost = await pool.query(
+      `INSERT INTO post(
+        author_name,
+        author_email,
+        text) VALUES ($1, $2, $3)
+        RETURNING *`,
+      [authorName, authorEmail, text],
+    );
+    res
+      .status(201)
+      .json({ key: newPost.rows[0] });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+});
